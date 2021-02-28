66670a1348779ff8dc1a2d8df00820c473b9eab0
acriscuoli <61270329+acriscuoli@users.noreply.github.com>
2021-02-28 22:44:54 +0100

    Delete product directory

--- product/src/main/java/com/example/product/product/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.product;
-
-public class ProductNotFoundException extends RuntimeException{
-	
-	ProductNotFoundException(Integer id) {
-	    super("Could not find product " + id);
-	  }
-
-}
